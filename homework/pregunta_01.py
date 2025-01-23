3627a34ec61e71a87cd0f9e4e1409fcc4d4961d0
dmolinav <dmolinav@unal.edu.co>
2025-01-23 03:51:54 -0500

    lab3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,9 +1,5 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
 
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
 
 def pregunta_01():
     """
@@ -15,6 +11,39 @@ def pregunta_01():
       espacios por guiones bajos.
     - Las palabras clave deben estar separadas por coma y con un solo
       espacio entre palabra y palabra.
-
-
     """
+    with open('files/input/clusters_report.txt', 'r', encoding='utf-8') as archivo:
+        todas_las_lineas = archivo.readlines()
+
+    with open('files/input/clusters_report.txt', 'r', encoding='utf-8') as archivo:
+        lineas_archivo = archivo.readlines()
+
+    lineas_datos = lineas_archivo[4:]
+
+    tabla_datos = [["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]]
+
+    fila_temporal = []
+    es_primera_linea = True
+
+    for linea_actual in lineas_datos:
+        linea_actual = linea_actual.strip()
+        linea_dividida = linea_actual.split()
+        if len(linea_dividida) > 0 and es_primera_linea:
+            fila_temporal.append(int(linea_dividida[0]))  # Número del clúster
+            fila_temporal.append(int(linea_dividida[1]))  # Cantidad de palabras clave
+            fila_temporal.append(float(linea_dividida[2].replace(',', '.')))  # Porcentaje de palabras clave
+            fila_temporal.append(" ".join(linea_dividida[4:]))  # Principales palabras clave
+            es_primera_linea = False
+
+        elif len(linea_dividida) > 0:
+            fila_temporal.append(" ".join(linea_dividida))
+
+        else:
+          es_primera_linea = True
+          fila_temporal[3] = ' '.join(fila_temporal[3:]).replace('.', '')  # Unir las palabras clave
+          tabla_datos.append(fila_temporal[:4])  # Agregar fila procesada
+          fila_temporal = []  # Reiniciar fila temporal
+
+    return(pd.DataFrame(tabla_datos[1:], columns=tabla_datos[0]))
+
+print(pregunta_01())
